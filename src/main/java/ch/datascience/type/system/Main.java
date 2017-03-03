d0ada8f518b291c7c8be331e2059f2e3db5b9a8c
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-03-03 14:56:50 +0100

    Source dirs created

---
+++ src/main/java/ch/datascience/type/system/Main.java
@@ -0,0 +1,12 @@
+package ch.datascience.type.system;
+
+/**
+ * Created by johann on 03/03/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ src/test/java/ch/datascience/type/system/TestMain.java
@@ -0,0 +1,14 @@
+package ch.datascience.type.system;
+
+import org.junit.*;
+
+/**
+ * Created by johann on 03/03/17.
+ */
+public class TestMain {
+
+    @Test
+    public void testOK() {
+    }
+
+}
