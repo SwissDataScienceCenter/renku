87470fd1c29b30cf727850194ddffea513eb933f
erbou <eric.bouillet@epfl.ch>
2017-04-13 00:06:37 +0200

    rabbitmq intg

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,15 @@
 package controllers
 
+import collection.JavaConversions._
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.json.JsValue
+
+import com.rabbitmq.client.ConnectionFactory
+import com.rabbitmq.client.Connection
+import com.rabbitmq.client.Channel
+import com.rabbitmq.client.AMQP.BasicProperties
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -10,6 +17,34 @@ import play.api.mvc._
  */
 @Singleton
 class HomeController @Inject() extends Controller {
+
+  val exchangeName = "sys"
+
+  val queueName = "kg"
+
+  val routingKey = "kgwal"
+
+  val connection = conn()
+
+  /**
+   * Initialize the rabbitmq client
+   */
+  def conn() : Connection = {
+    println("Create connection")
+    val connFactory = new ConnectionFactory()
+    connFactory.setUsername("guest")
+    connFactory.setPassword("guest")
+    connFactory.setVirtualHost("/")
+    connFactory.setHost("iccluster108.iccluster.epfl.ch")
+    connFactory.setPort(5672)
+    val conn = connFactory.newConnection()
+    val chan = conn.createChannel()
+    chan.exchangeDeclare(exchangeName, "direct", true)
+    chan.queueDeclare(queueName, true, false, false, null)
+    chan.queueBind(queueName, exchangeName, routingKey)
+    chan.close()
+    return conn
+  }
 
   /**
    * Create an Action to render an HTML page.
@@ -20,5 +55,28 @@ class HomeController @Inject() extends Controller {
    */
   def index = Action { implicit request =>
     Ok(views.html.index())
+  }
+
+  def event = Action { implicit request =>
+    val body: AnyContent = request.body
+    val jsonBody: Option[JsValue] = body.asJson
+
+    // Expecting json body (Content-Type: application/json)
+    jsonBody.map { json =>
+      val oid = (json \ "oid").as[String]
+      val evt = (json \ "evt").as[JsValue]
+      val chan = this.connection.createChannel()
+      println("Received event: " + oid + ", " + evt)
+      val headers = mapAsJavaMap(Map(("id",oid))).asInstanceOf[java.util.Map[String,Object]]
+      chan.basicPublish(exchangeName, routingKey, new BasicProperties.Builder()
+               .headers(headers)
+               .contentType("application/json")
+               .build(), evt.toString().getBytes()
+         )
+      chan.close()
+      Ok("Got: " + oid + ", " + evt)
+    }.getOrElse {
+      BadRequest("Expecting application/json request body")
+    }
   }
 }
--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+/// Modified version of automatically generated build configs from sbt new playframework/play-scala-seed.g8
+///
 name := """graph-wal"""
 organization := "ch.datascience"
 
@@ -10,10 +12,19 @@ scalaVersion := "2.11.8"
 libraryDependencies += filters
 libraryDependencies ++= Seq(
   "com.typesafe.slick" %% "slick" % "3.2.0",
-  "com.rabittmq" %% "amqp-client" % "4.1.0",
+  "com.rabbitmq" % "amqp-client" % "4.1.0",
   "org.slf4j" % "slf4j-nop" % "1.7.24",
   "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.0" % Test
 )
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "datascience.ch.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "datascience.ch.binders._"
+
+// HTTP server port, will be ignored in production mode, use system settings instead
+PlayKeys.devSettings := Seq("play.server.http.port" -> "9001")
 
 resolvers ++= Seq(
   DefaultMavenRepository,
