dd0c2431c517393d890ff4758460253ccfa6d280
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-04-27 17:38:19 +0200

    Added cardinality to VertexPropertyValues

--- src/main/scala/ch/datascience/graph/elements/VertexPropertyValues.scala
+++ src/main/scala/ch/datascience/graph/elements/VertexPropertyValues.scala
@@ -1,5 +1,7 @@
 package ch.datascience.graph.elements
 
+import ch.datascience.graph.types.Cardinality
+
 /**
   * Created by johann on 27/04/17.
   */
@@ -18,6 +20,12 @@ sealed abstract class VertexPropertyValues[Key, Value : ValidValue, MetaKey] ext
     case SingleValue(vp) => SingleValue(vp.boxed)
     case SetValue(map) => SetValue((for (vp <- map.values) yield vp.boxedValue -> vp.boxed).toMap)
     case ListValue(vps) => ListValue(vps.map(_.boxed))
+  }
+
+  final def cardinality: Cardinality = this match {
+    case SingleValue(_) => Cardinality.Single
+    case SetValue(_) => Cardinality.Set
+    case ListValue(_) => Cardinality.List
   }
 
 }
