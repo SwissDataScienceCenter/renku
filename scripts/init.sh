e895609003bdf1ddd60f6b8b1463eb184f660e5b
Eric Bouillet <eric.bouillet@epfl.ch>
2017-05-05 16:41:09 +0200

    clone script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Clone graph-core repository
-if [ ! -d "graph-core" ]; then
-  git clone git@github.com:SwissDataScienceCenter/graph-core.git
+if [ ! -e "graph-core/build.sbt" ]; then
+  rm -rf graph-core/target
+  git clone git@github.com:SwissDataScienceCenter/graph-core.git tmp
+  rsync -rtv tmp/ graph-core/
+  rm -rf tmp
 fi
+
