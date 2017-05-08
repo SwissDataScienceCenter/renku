53d13347d458490a07d4b85017b947d64df3e059
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-05-08 10:58:01 +0200

    Refactored for better subproject management

--- build.sbt
+++ build.sbt
@@ -14,12 +14,26 @@ lazy val core = Project(
   scriptsSettings
 )
 
+lazy val typesystem = Project(
+  id   = "graph-typesystem",
+  base = file("graph-typesystem")
+).settings(
+  commonSettings,
+  scriptsSettings
+).dependsOn(core)
+
 lazy val initProjects = taskKey[Unit]("Execute the init script")
 lazy val updateProjects = taskKey[Unit]("Execute the pull script")
 
 lazy val scriptsSettings = Seq(
-  initProjects := { "scripts/init.sh" ! }
-, updateProjects := { "scripts/pull.sh" !}
+  initProjects := {
+    println(s"Calling: scripts/init.sh ${name.value} ${baseDirectory.value}")
+    s"scripts/init.sh ${name.value} ${baseDirectory.value}" !
+  }
+, updateProjects := {
+    println(s"Calling: scripts/pull.sh ${name.value} ${baseDirectory.value}")
+    s"scripts/pull.sh ${name.value} ${baseDirectory.value}" !
+  }
 , updateProjects := (updateProjects dependsOn initProjects).value
 , compile in Compile := ((compile in Compile) dependsOn initProjects).value
 )
--- scripts/init.sh
+++ scripts/init.sh
@@ -4,11 +4,37 @@ log() {
     printf "%s\n" "$*" >&2;
 }
 
-log 'init'
+# Clone a given repository, $1=project name, $2=git base url, $3=root directory
+clone() {
+    # Name arguments
+    name=$1
+    baseUrl=$2
+    rootDir=$3
 
-# Clone graph-core repository
-if [ ! -d "graph-core/.git" ]; then
-  git clone git@github.com:SwissDataScienceCenter/graph-core.git tmp-graph-core
-  rsync -a --ignore-existing tmp-graph-core/ graph-core/
-  rm -rf tmp-graph-core
+    repoDir="$rootDir/$name"
+    repoUrl="$baseUrl/$name.git"
+    tempDir="$rootDir/tmp-$1"
+
+    if [ ! -d "$repoDir/.git" ]; then
+        log "Init project: $name"
+        git clone $repoUrl $tempDir
+        rsync -a --ignore-existing "$tempDir/" "$repoDir/"
+        rm -rf $tempDir
+    fi
+}
+
+# Check if a project name and a project directory are provided
+if [ ! $# -eq 2 ]; then
+    log "Usage ${BASH_SOURCE} project projectDirectory"
+    exit 1
 fi
+
+project=$1
+projectDir=$2
+
+baseDir="$2/.."
+
+# Init variables
+source "${BASH_SOURCE%/*}/project.env"
+
+clone $project $BASE_URL $baseDir
