1fdd108d56190c112898fc1ed15171664730c113
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-05-09 11:02:25 +0200

    Fixed minor issues with git submodule

--- scripts/update.sh
+++ scripts/update.sh
@@ -8,20 +8,22 @@ update() {
     # Name arguments
     name=$1
 
-    rootDir="${BASH_SOURCE%/*}/.."
+    rootDir=$(readlink -qe "${BASH_SOURCE%/*}/..")
     repoDir="$rootDir/$name"
 
     # Check if sbt made non empty subproject directories (target non empty)
     if [ -d $repoDir ] && [ ! -e "$repoDir/.git" ]; then
         log "$repoDir is non empty, removing..."
+        mv $repoDir "to-remove-$repoDir"
         rm -rf $repoDir
     fi
 
-    git submodule update --init --recursive $1
+    git submodule update --remote --init --recursive --merge $1
 }
 
 if [ ! $# -eq 1 ]; then
     log "Usage: ${BASH_SOURCE} <project>"
+    exit 1
 fi
 
 # Name arguments
