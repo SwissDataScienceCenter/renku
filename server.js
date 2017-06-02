13b2c045de29f1008353dc48ca3cd327af50cf1d
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2017-06-02 16:51:44 +0200

    Making it more convenient to write APIs

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use('/rest-api-definitions', express.static('.'))
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
