92dfeedbdbf77a05df404f9f946a6e15266eb3fb
Jiri Kuncar <jiri.kuncar@gmail.com>
2017-08-24 15:39:06 +0200

    Use Renga prefixes and names
    * NOTE Makes demo-client on Keycloak public.

--- example/functions.sh
+++ example/functions.sh
@@ -9,14 +9,14 @@ function getAccessToken() {
     JQ_CMD="jq -r \".access_token\""
     CMD="$CURL_CMD | $JQ_CMD"
     echo $CMD
-    SDSC_ACCESS_TOKEN=$(eval $CMD)
+    RENGA_ACCESS_TOKEN=$(eval $CMD)
 }
 
 function getFirstBucket() {
     echo "Listing buckets..."
-    CURL_CMD="curl -s -X GET \"$SDSC_API_URL/api/storage/explore/list\" \
+    CURL_CMD="curl -s -X GET \"$RENGA_API_URL/api/storage/explore/list\" \
         -H \"accept: application/json\" \
-        -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\""
+        -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\""
     JQ_CMD="jq \".[0].id\""
     CMD="$CURL_CMD | $JQ_CMD"
     echo $CMD
@@ -26,10 +26,10 @@ function getFirstBucket() {
 
 function createBucket() {
     echo "Creating bucket 'my_bucket' ..."
-    CURL_CMD="curl -s -X POST \"$SDSC_API_URL/api/storage/authorize/create_bucket\" \
+    CURL_CMD="curl -s -X POST \"$RENGA_API_URL/api/storage/authorize/create_bucket\" \
         -H \"accept: application/json\" \
         -H \"content-type: application/json\" \
-        -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\" \
+        -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\" \
         -d \"{ \\\"name\\\": \\\"my_bucket\\\", \\\"backend\\\": \\\"local\\\", \\\"request_type\\\": \\\"create_bucket\\\"}\""
     JQ_CMD="jq \".response.event.results[0].id\""
     CMD="$CURL_CMD | $JQ_CMD"
@@ -41,9 +41,9 @@ function createBucket() {
 function listFiles() {
     local bucket=$1
     echo "Listing files in '$bucket' ..."
-    CURL_CMD="curl -s -X GET \"$SDSC_API_URL/api/storage/explore/$bucket/files\" \
+    CURL_CMD="curl -s -X GET \"$RENGA_API_URL/api/storage/explore/$bucket/files\" \
         -H \"accept: application/json\" \
-        -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\""
+        -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\""
     JQ_CMD="jq \"[.[] | {file_id: .id, file_name: .properties | map(select(.key == \\\"resource:file_name\\\")) | .[0].values[0].value }]\""
     CMD="$CURL_CMD | $JQ_CMD"
     echo $CMD
@@ -67,14 +67,14 @@ function authReadFile() {
   local file_id=$1
   echo "Writing to file '$file_id' ..."
   local deployment_id_curl=""
-  if [ -n "$SDSC_DEPLOYMENT_ID" ]; then
-      deployment_id_curl="-H \"SDSC-DEPLOYMENT-ID: $SDSC_DEPLOYMENT_ID\""
+  if [ -n "$RENGA_DEPLOYMENT_ID" ]; then
+      deployment_id_curl="-H \"Renga-DEPLOYMENT-ID: $RENGA_DEPLOYMENT_ID\""
   fi
-  CURL_CMD="curl -s -X POST \"$SDSC_API_URL/api/storage/authorize/read\" \
+  CURL_CMD="curl -s -X POST \"$RENGA_API_URL/api/storage/authorize/read\" \
       -H \"accept: application/json\" \
       -H \"content-type: application/json\" \
       $deployment_id_curl \
-      -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\" \
+      -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\" \
       -d \"{ \\\"resource_id\\\": $file_id, \\\"request_type\\\": \\\"read_file\\\"}\""
   JQ_CMD="jq -r \".access_token\""
   CMD="$CURL_CMD | $JQ_CMD"
@@ -88,14 +88,14 @@ function createFile() {
     local filename=$2
     echo "Creating file '$bucket/$filename' ..."
     local deployment_id_curl=""
-    if [ -n "$SDSC_DEPLOYMENT_ID" ]; then
-        deployment_id_curl="-H \"SDSC-DEPLOYMENT-ID: $SDSC_DEPLOYMENT_ID\""
+    if [ -n "$RENGA_DEPLOYMENT_ID" ]; then
+        deployment_id_curl="-H \"Renga-DEPLOYMENT-ID: $RENGA_DEPLOYMENT_ID\""
     fi
-    CURL_CMD="curl -s -X POST \"$SDSC_API_URL/api/storage/authorize/create_file\" \
+    CURL_CMD="curl -s -X POST \"$RENGA_API_URL/api/storage/authorize/create_file\" \
         -H \"accept: application/json\" \
         -H \"content-type: application/json\" \
         $deployment_id_curl \
-        -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\" \
+        -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\" \
         -d \"{ \\\"bucket_id\\\": $bucket, \\\"file_name\\\": \\\"$filename\\\", \\\"request_type\\\": \\\"create_file\\\"}\""
     JQ_CMD="jq -r \".access_token\""
     CMD="$CURL_CMD | $JQ_CMD"
@@ -108,14 +108,14 @@ function authWriteFile() {
     local file_id=$1
     echo "Writing to file '$file_id' ..."
     local deployment_id_curl=""
-    if [ -n "$SDSC_DEPLOYMENT_ID" ]; then
-        deployment_id_curl="-H \"SDSC-DEPLOYMENT-ID: $SDSC_DEPLOYMENT_ID\""
+    if [ -n "$RENGA_DEPLOYMENT_ID" ]; then
+        deployment_id_curl="-H \"Renga-DEPLOYMENT-ID: $RENGA_DEPLOYMENT_ID\""
     fi
-    CURL_CMD="curl -s -X POST \"$SDSC_API_URL/api/storage/authorize/write\" \
+    CURL_CMD="curl -s -X POST \"$RENGA_API_URL/api/storage/authorize/write\" \
         -H \"accept: application/json\" \
         -H \"content-type: application/json\" \
         $deployment_id_curl \
-        -H \"Authorization: Bearer $SDSC_ACCESS_TOKEN\" \
+        -H \"Authorization: Bearer $RENGA_ACCESS_TOKEN\" \
         -d \"{ \\\"resource_id\\\": $file_id, \\\"request_type\\\": \\\"write_file\\\"}\""
     JQ_CMD="jq -r \".access_token\""
     CMD="$CURL_CMD | $JQ_CMD"
@@ -145,7 +145,7 @@ function authWriteOrCreateFile() {
 function readFile() {
     local auth_token=$1
     echo "Reading data..."
-    CURL_CMD="curl -s -X GET \"$SDSC_API_URL/api/storage/io/read\" \
+    CURL_CMD="curl -s -X GET \"$RENGA_API_URL/api/storage/io/read\" \
         -H \"Authorization: Bearer $auth_token\""
     CMD="$CURL_CMD"
     echo $CMD
@@ -157,7 +157,7 @@ function writeFile() {
     local auth_token=$1
     local data=$2
     echo "Writing data..."
-    CURL_CMD="curl -s -X POST \"$SDSC_API_URL/api/storage/io/write\" \
+    CURL_CMD="curl -s -X POST \"$RENGA_API_URL/api/storage/io/write\" \
         -H \"accept: application/json\" \
         -H \"content-type: text/plain\" \
         -H \"Authorization: Bearer $auth_token\"
