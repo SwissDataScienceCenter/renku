a7e92422241dc0f9bc5cb1385b3da0c3560d8058
Rok Roskar <roskarr@ethz.ch>
2017-08-28 14:49:11 +0200

    add checks on service startup

---
+++ travis-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Swiss Data Science Center
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]
+then
+    make
+else
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY
+    docker-compose pull
+fi
