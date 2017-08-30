0a3f4033c0f67f346b945dddb285b12c4538c6d3
Eric Bouillet <eric.bouillet@epfl.ch>
2017-08-30 13:59:10 +0200

    fix git deploy script

--- .travis/git-commit-build.sh
+++ .travis/git-commit-build.sh
@@ -2,5 +2,5 @@
 
 git add --force resources/uml/*.png
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} from ${TRAVIS_BRANCH}/${TRAVIS_COMMIT}"
-git push --force origin travis-build
+git push --force origin HEAD:travis-build
 
