27888b1be062888c52c20d49ae4f5d556a238968
Eric Bouillet <eric.bouillet@epfl.ch>
2017-08-30 14:23:44 +0200

    mv pictures to source/images/generated

--- .travis/git-commit-build.sh
+++ .travis/git-commit-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add --force resources/uml/*.png
+git add --force source/images/generated
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} from ${TRAVIS_BRANCH}/${TRAVIS_COMMIT}"
 git push --force origin HEAD:travis-build
 
