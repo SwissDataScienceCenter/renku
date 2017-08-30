26fe6cf1ff98379e2cd962222953b4056d6b80a9
Eric Bouillet <eric.bouillet@epfl.ch>
2017-08-30 12:32:13 +0200

    fix path error, add resources/_static

--- .travis/git-commit-build.sh
+++ .travis/git-commit-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git add --force resource/uml/*.png
+git add --force resources/uml/*.png
 git commit -m "Travis build ${TRAVIS_BUILD_NUMBER} from ${TRAVIS_BRANCH}/${TRAVIS_COMMIT}"
 git push --force origin travis-build
 
