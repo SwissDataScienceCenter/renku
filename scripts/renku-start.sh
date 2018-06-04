7ad26e8ee7919b11217aeb75f76b5c626af47df6
Rok Roskar <roskarr@ethz.ch>
2018-06-05 01:13:56 +0200

    compose: fix runner configuration
    Mount docker socket for image builds.

--- scripts/renku-start.sh
+++ scripts/renku-start.sh
@@ -83,16 +83,16 @@ registerGitlabRunners() {
     do
         docker exec -ti $container gitlab-runner register \
                 -n -u ${GITLAB_URL} \
-                --name $$container-shell \
+                --name $$container-docker \
                 -r ${GITLAB_RUNNERS_TOKEN} \
                 --executor docker \
-                --env RENKU_REVIEW_DOMAIN=${PLATFORM_DOMAIN} \
-                --env RENKU_RUNNER_NETWORK=${DOCKER_NETWORK} \
                 --locked=false \
                 --run-untagged=true \
                 --docker-image "docker:stable" \
                 --docker-network-mode=review \
                 --docker-privileged \
+                --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
+                --tag-list image-build \
                 --docker-pull-policy "if-not-present"; \
     done
 }
