7d205caa4d50c174dcbf5eeb4c97aedffcde2ad3
Johann-Michael Thiebaut <johann-michael.thiebaut@epfl.ch>
2018-07-31 11:06:48 +0200

    fix typo

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -25,7 +25,7 @@ set -o nounset
 
 docker images --digests
 
-helm lint charts/renku -f chatts/minikube-values.yaml
+helm lint charts/renku -f charts/minikube-values.yaml
 helm test renku
 
 # pytest -v
