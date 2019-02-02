87e82f784a596b1b8ec3de3db279227fe025211b
Rok Roškar <rokroskar@gmail.com>
2019-02-02 11:37:41 +0100

    Update travis-install.sh

--- scripts/travis-install.sh
+++ scripts/travis-install.sh
@@ -33,7 +33,7 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.10
 chmod +x kubectl
 sudo mv kubectl /usr/local/bin/
 # install helm
-curl -Lo helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz
+curl -Lo helm.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-linux-amd64.tar.gz
 tar xvf helm.tar.gz
 chmod +x linux-amd64/helm
 sudo mv linux-amd64/helm /usr/local/bin/
