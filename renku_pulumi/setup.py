e8aae15a01884a7d40e2bf6ad9d5eb1d81ff9077
Panaetius <ralf.grubenmann@sdsc.ethz.ch>
2019-12-11 11:26:01 +0100

    Change to proper python package

---
+++ renku_pulumi/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='renku_pulumi',
+      version='1.0',
+      description='Renku Pulumi deployment',
+      author='Ralf Grubenmann',
+      author_email='ralf.grubenmann@sdsc.ethz.ch',
+      url='https://github.com/SwissDataScienceCenter/renku',
+      packages=find_packages()
+     )
