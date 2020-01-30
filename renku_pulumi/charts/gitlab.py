71859c3b99c31be4ffda3d82798d74499abc0850
Ralf Grubenmann <ralf.grubenmann@sdsc.ethz.ch>
2020-01-30 17:29:39 +0100

    Simplifies gitlab and keycloak charts

--- renku_pulumi/charts/gitlab.py
+++ renku_pulumi/charts/gitlab.py
@@ -1,11 +1,30 @@
 import pulumi
 from pulumi_kubernetes.helm.v2 import Chart, ChartOpts
+from deepmerge import always_merger
 
-config = pulumi.Config()
+default_chart_values = {
+    "password": "gitlabadmin",
+    "image": {
+        "repository": "gitlab/gitlab-ce",
+        "tag": "11.8.10-ce.0"
+    },
+    "oauth":{
+      "autoSignIn": True
+    },
+    "demoUserIsAdmin": False,
+    "persistence": {
+        "size": "30Gi"
+    },
+    "registry": {
+        "enabled": False
+    }
+}
 
 def gitlab(config, global_config):
     gitlab_config = pulumi.Config('gitlab')
-    values = gitlab_config.require_object('values')
+    values = gitlab_config.get_object('values') or {}
+
+    values = always_merger.merge(default_chart_values, values)
 
     global_config = pulumi.Config('global')
     global_values = global_config.require_object('values')
--- renku_pulumi/charts/keycloak.py
+++ renku_pulumi/charts/keycloak.py
@@ -1,11 +1,84 @@
 import pulumi
 from pulumi_kubernetes.helm.v2 import Chart, ChartOpts
+from deepmerge import always_merger
 
-config = pulumi.Config()
+default_chart_values = {
+    "test": {
+        "enabled": False
+    },
+    "createDemoUser": False,
+    "initRealm": {
+        "image": {
+            "repository": "renku/init-realm",
+            "tag": "latest"
+        }
+    },
+    "keycloak": {
+        "existingSecret": "keycloak-password-secret",
+        "existingSecretKey": "keycloak-password",
+        "service": {
+            "type": "ClusterIP",
+            "port": 80
+        },
+        "test": {
+            "enabled": False
+        },
+        "livenessProbe": {
+            "initialDelaySeconds": 180
+        },
+        "readinessProbe": {
+            "initialDelaySeconds": 120
+        },
+        "extraInitContainers": """- name: theme-provider
+          image: renku/keycloak-theme:v1.2
+          imagePullPolicy: IfNotPresent
+          command:
+            - sh
+          args:
+            - -c
+            - |
+              echo \"Copying theme...\"
+              cp -Rfv /renku_theme/* /theme
+          volumeMounts:
+            - name: theme
+              mountPath: /theme""",
+        "username": "admin",
+        "persistence": {
+            "deployPostgres": False,
+            "dbVendor": "postgres",
+            "dbName": "keycloak",
+            "dbPort": 5432,
+            "dbUser": "keycloak",
+            "existingSecret": "renku-keycloak-postgres",
+            "existingSecretKey": "keycloak-postgres-password"
+        },
+        "extraEnv": """
+        - name: PROXY_ADDRESS_FORWARDING
+          value: \"true\"""",
+        "extraVolumes": """
+        - name: theme
+          emptyDir: {{}}""",
+        "extraVolumeMounts": """
+        - name: theme
+          mountPath: /opt/jboss/keycloak/themes/renku-theme""",
+        "ingress": {
+            "enabled": False
+        }
+    },
+    "resource": {
+        "requests": {
+            "cpu": "1000m",
+            "memory": "2Gi"
+        }
+    }
+}
+
 
 def keycloak(config, global_config, dependencies=[]):
     keycloak_config = pulumi.Config('keycloak')
-    values = keycloak_config.require_object('values')
+    values = keycloak_config.get_object('values') or {}
+
+    values = always_merger.merge(default_chart_values, values)
 
     global_values = pulumi.Config('global')
     global_values = global_values.require_object('values')
