6625b9a55c89b79679893ed69f93013454c2ae67
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2020-02-21 16:22:13 +0100

    build: update lock file

--- actions/update-component-version/update-upstream.sh
+++ actions/update-component-version/update-upstream.sh
@@ -24,6 +24,16 @@ then
   CHART_TAG="--tag $(echo ${GITHUB_REF} | cut -d/ -f3)"
 fi
 
+# install helm
+HELM_URL=${HELM_URL:=https://storage.googleapis.com/kubernetes-helm}
+HELM_TGZ=${HELM_TGZ:=helm-v2.14.3-linux-amd64.tar.gz}
+
+mkdir -p /tmp/helm
+wget -q ${HELM_URL}/${HELM_TGZ} -O /tmp/helm/${HELM_TGZ}
+tar -C /tmp/helm -xzv -f /tmp/helm/${HELM_TGZ}
+PATH=/tmp/helm/linux-amd64/:$PATH
+helm init --client-only
+
 # build this chart to get the version
 chartpress --skip-build $CHART_TAG
 CHART_VERSION=$(yq r helm-chart/${CHART_NAME}/Chart.yaml version)
@@ -40,7 +50,12 @@ git config --global user.name "Renku Bot"
 # update the chart requirements and push
 git checkout -b auto-update/${CHART_NAME}-${CHART_VERSION} master
 yq w -i charts/renku/requirements.yaml "dependencies.(name==${CHART_NAME}).version" $CHART_VERSION
-git commit -am "chore: updating ${CHART_NAME} version to ${CHART_VERSION}"
+helm repo add renku https://swissdatasciencecenter.github.io/helm-charts/
+helm dep update charts/renku
+
+git add charts/renku/requirements.yaml
+git add charts/renku/requirements.lock
+git commit -m "chore: updating ${CHART_NAME} version to ${CHART_VERSION}"
 git push origin auto-update/${CHART_NAME}-${CHART_VERSION}
 
 # clean up
