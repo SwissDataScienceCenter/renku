7be8d89fb41aef00c6be78ef0c84ade80dfb9c92
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2020-04-28 15:55:30 +0200

    added TestsDefaults; reorganized readme (#1039)
    * refactor: added TestsDefaults; reorganized readme
    
    fix: around tests-default.conf
    
    * style: added newline to end of tests-defaults.conf.template
    
    Co-authored-by: Chandrasekhar Ramakrishnan <cramakri@ethz.ch>

---
+++ tests/src/test/scala/ch/renku/acceptancetests/tooling/TestsDefaults.scala
@@ -0,0 +1,22 @@
+package ch.renku.acceptancetests.tooling
+
+case class TestsDefaults(env:      Option[String],
+                         email:    Option[String] = None,
+                         username: Option[String] = None,
+                         password: Option[String] = None,
+                         fullname: Option[String] = None)
+
+object TestsDefaults {
+  import java.nio.file.Paths
+
+  import pureconfig.ConfigSource
+  import pureconfig.generic.auto._
+
+  private val defaultConfigFileName = "./tests-defaults.conf"
+
+  def apply(): TestsDefaults =
+    if (Paths.get(defaultConfigFileName).toFile.exists())
+      ConfigSource.file(defaultConfigFileName).load[TestsDefaults].getOrElse(TestsDefaults(Some("https://dev.renku.ch")))
+    else
+      TestsDefaults(Some("https://dev.renku.ch"))
+}
