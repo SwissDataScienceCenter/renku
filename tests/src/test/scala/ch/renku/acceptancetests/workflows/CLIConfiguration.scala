b849f61e89924683f957ba37bd6ed3ae1d458302
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2020-08-21 19:01:11 +0200

    chore: merge ui changes and update integration test (#1389)
    fix #1249
    
    Co-authored-by: David Kunzmann <davidkunzmann@gmail.com>
    Co-authored-by: Rok Roškar <roskarr@ethz.ch>
    Co-authored-by: Travis Lee <travis.lee@sdsc.ethz.ch>

--- tests/src/test/scala/ch/renku/acceptancetests/workflows/CLIConfiguration.scala
+++ tests/src/test/scala/ch/renku/acceptancetests/workflows/CLIConfiguration.scala
@@ -4,7 +4,7 @@ import java.nio.file.Path
 
 import ch.renku.acceptancetests.model.users.UserCredentials
 import ch.renku.acceptancetests.tooling.console._
-import ch.renku.acceptancetests.tooling.{RenkuCliConfig, console}
+import ch.renku.acceptancetests.tooling.console
 import org.scalatest.{FeatureSpec, GivenWhenThen}
 
 trait CLIConfiguration extends GivenWhenThen {
@@ -20,14 +20,5 @@ trait CLIConfiguration extends GivenWhenThen {
 
     val configuredEmail: String = console %%> c"git config --global user.email"
     if (configuredEmail.trim.isEmpty) console %> c"git config --global user.email '${userCredentials.email}'"
-  }
-
-  def `verify renku version`(implicit cliConfig: RenkuCliConfig, userCredentials: UserCredentials): Unit = {
-    implicit val workFolder: Path = rootWorkDirectory
-
-    When(s"the user has renku v${cliConfig.version} installed")
-    val installedRenkuVersion = console %%> c"renku --version"
-    if (installedRenkuVersion.trim != cliConfig.version.toString)
-      console %> c"${cliConfig.installCommand.toString.format(cliConfig.version.toString)}"
   }
 }
