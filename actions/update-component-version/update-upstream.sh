52ba4a9bc5082b5e36fbae0d532ae682711d18a1
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2020-10-23 19:03:13 +0200

    chore: remove helm from update-component action (#1616)
    * fix: removed helm from update-component-version action

--- actions/update-component-version/update-upstream.sh
+++ actions/update-component-version/update-upstream.sh
@@ -18,16 +18,6 @@ GIT_EMAIL=${GIT_EMAIL:=renku@datascience.ch}
 GIT_USER=${GIT_USER:="Renku Bot"}
 CHART_NAME=${CHART_NAME:=$(echo $GITHUB_REPOSITORY | cut -d/ -f2)}
 
-# install helm
-HELM_URL=${HELM_URL:=https://storage.googleapis.com/kubernetes-helm}
-HELM_TGZ=${HELM_TGZ:=helm-v2.16.1-linux-amd64.tar.gz}
-
-mkdir -p /tmp/helm
-wget -q ${HELM_URL}/${HELM_TGZ} -O /tmp/helm/${HELM_TGZ}
-tar -C /tmp/helm -xzv -f /tmp/helm/${HELM_TGZ}
-PATH=/tmp/helm/linux-amd64/:$PATH
-helm init --client-only
-
 # build this chart to get the version
 chartpress --skip-build $CHART_TAG
 CHART_VERSION=$(yq r helm-chart/${CHART_NAME}/Chart.yaml version)
@@ -44,8 +34,6 @@ git config --global user.name "$GIT_USER"
 # update the chart requirements and push
 git checkout -b auto-update/${CHART_NAME}-${CHART_VERSION} master
 yq w -i charts/renku/requirements.yaml "dependencies.(name==${CHART_NAME}).version" $CHART_VERSION
-helm repo add renku https://swissdatasciencecenter.github.io/helm-charts/
-helm dep update charts/renku
 
 git add charts/renku/requirements.yaml
 git commit -m "chore: updating ${CHART_NAME} version to ${CHART_VERSION}"
