285781365d36cd9c33c74ebb893ed81bef5d9e80
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2020-11-30 22:54:25 +0100

    chore: define renku deploy action

--- scripts/deploy_dev_renku.py
+++ scripts/deploy_dev_renku.py
@@ -1,18 +1,15 @@
-#!/bin/env python
+#!/usr/bin/env python3
 #
-# Usage: Specify versions of components and other deployment details via environment variables.
+# Usage: ./deploy_dev_renku -h
 #
-# Example:
+# Note: you can use the following environment variables to set defaults:
 #
-# RENKU_VALUES_FILE=<path-to-values-file> \
-# RENKU_RELEASE=<namespace>-renku \
-# RENKU_RELEASE_NAMESPACE=<namespace> \
-# renku_core="@<git-ref>" renku_ui="0.11.0" python3 deploy_dev_renku.py
-#
+# RENKU_VALUES_FILE
+# RENKU_RELEASE
+# RENKU_NAMESPACE
 #
 
-
-#%%
+import argparse
 import os
 import pprint
 import re
@@ -40,12 +37,16 @@ class RenkuRequirement(object):
         self.tempdir = tempdir
 
         self.version_ = version
-        self.ref = None
         self.is_git_ref = False
         if version.startswith("@"):
             # this is a git ref
-            self.ref = version
             self.is_git_ref = True
+
+    @property
+    def ref(self):
+        if self.is_git_ref:
+            return self.version_.strip("@")
+        return None
 
     @property
     def version(self):
@@ -93,7 +94,7 @@ class RenkuRequirement(object):
                     self.repo_dir,
                 ]
             )
-        check_call(["git", "checkout", self.ref.strip("@")], cwd=self.repo_dir)
+        check_call(["git", "checkout", self.ref], cwd=self.repo_dir)
 
     def chartpress(self, skip_build=False):
         """Run chartpress."""
@@ -108,23 +109,20 @@ class RenkuRequirement(object):
     def setup(self):
         """Checkout the repo and run chartpress."""
         self.clone()
-        self.chartpress()
+        self.chartpress(skip_build=True)
 
 
-def configure_requirements(tempdir, reqs, components):
+def configure_requirements(tempdir, reqs, component_versions):
     """
     Reads versions from environment variables and renders the requirements.yaml file.
 
     If any of the requested versions reference a git ref, the chart is rendered and
     images built and pushed to dockerhub.
     """
-    for component in components:
-        if os.environ.get(component.replace("-", "_")):
-            # we have specified a component to override
-            version = os.environ.get(component.replace("-", "_"))
-
+    for component, version in component_versions.items():
+        if version:
             # form and setup the requirement
-            req = RenkuRequirement(component, version, tempdir)
+            req = RenkuRequirement(component.replace("_", "-"), version, tempdir)
             if req.ref:
                 req.setup()
                 # replace the requirement
@@ -136,48 +134,83 @@ def configure_requirements(tempdir, reqs, components):
     return reqs
 
 
-tempdir_ = tempfile.TemporaryDirectory()
-tempdir = Path(tempdir_.name)
+if __name__ == "__main__":
+    from argparse import ArgumentParser
 
-renku_dir = os.environ.get("RENKU_DIR", tempdir / "renku")
-reqs_path = renku_dir / "charts/renku/requirements.yaml"
-
-## 1. clone the renku repo
-check_call(["git", "clone", "https://github.com/SwissDataScienceCenter/renku.git", renku_dir])
-
-with open(reqs_path) as f:
-    reqs = yaml.load(f)
-
-## 2. set the chosen versions in the requirements.yaml file
-reqs = configure_requirements(tempdir, reqs, components)
-
-with open(reqs_path, "w") as f:
-    yaml.dump(reqs, f)
-
-## 3. run helm dep update renku
-check_call(["helm3", "dep", "update", "renku"], cwd=renku_dir / "charts")
-
-## 4. deploy
-values_file = os.environ.get("RENKU_VALUES_FILE")
-release = os.environ.get("RENKU_RELEASE")
-release_namespace = os.environ.get("RENKU_RELEASE_NAMESPACE")
-
-print(f"*** Dependencies for release {release} under namespace {release_namespace} ***")
-pprint.pp(reqs)
-
-check_call(
-    [
-        "helm3",
-        "upgrade",
-        "--install",
-        release,
-        "./renku",
-        "-f",
-        values_file,
+    parser = ArgumentParser()
+    for component in components:
+        parser.add_argument(f"--{component}", help=f"Version or ref for {component}")
+    parser.add_argument("--renku", help="Main chart ref")
+    parser.add_argument(
+        "--values-file",
+        help="Value file path",
+        default=os.environ.get("RENKU_VALUES_FILE"),
+    )
+    parser.add_argument(
         "--namespace",
-        release_namespace,
-    ],
-    cwd=renku_dir / "charts",
-)
+        help="Namespace for this release",
+        default=os.environ.get("RENKU_NAMESPACE"),
+    )
+    parser.add_argument(
+        "--release", help="Release name", default=os.environ.get("RENKU_RELEASE")
+    )
 
-tempdir_.cleanup()
+    args = parser.parse_args()
+    component_versions = {
+        a: b for a, b in vars(args).items() if a.replace("_", "-") in components
+    }
+
+    tempdir_ = tempfile.TemporaryDirectory()
+    tempdir = Path(tempdir_.name)
+
+    renku_dir = tempdir / "renku"
+    reqs_path = renku_dir / "charts/renku/requirements.yaml"
+
+    ## 1. clone the renku repo
+    check_call(
+        [
+            "git",
+            "clone",
+            "https://github.com/SwissDataScienceCenter/renku.git",
+            renku_dir,
+        ]
+    )
+    if args.renku:
+        check_call(["git", "checkout", args.renku], cwd=renku_dir)
+
+    with open(reqs_path) as f:
+        reqs = yaml.load(f)
+
+    ## 2. set the chosen versions in the requirements.yaml file
+    reqs = configure_requirements(tempdir, reqs, component_versions)
+
+    with open(reqs_path, "w") as f:
+        yaml.dump(reqs, f)
+
+    ## 3. run helm dep update renku
+    check_call(["helm3", "dep", "update", "renku"], cwd=renku_dir / "charts")
+
+    ## 4. deploy
+    values_file = args.values_file
+    release = args.release
+    namespace = args.namespace
+
+    print(f'*** Dependencies for release "{release}" under namespace "{namespace}" ***')
+    pprint.pp(reqs)
+
+    check_call(
+        [
+            "helm3",
+            "upgrade",
+            "--install",
+            release,
+            "./renku",
+            "-f",
+            values_file,
+            "--namespace",
+            namespace,
+        ],
+        cwd=renku_dir / "charts",
+    )
+
+    tempdir_.cleanup()
