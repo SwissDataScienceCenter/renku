bc59fdcb9e830e95acd7902248a2a90df5ca93d4
Rok Roskar <rok.roskar@sdsc.ethz.ch>
2020-12-04 18:19:02 +0100

    chore: containerize teardown action
    closes #1772

---
+++ actions/teardown-renku/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# set up kube context and values file
+echo "$RENKUBOT_KUBECONFIG" > "$KUBECONFIG" && chmod 400 "$KUBECONFIG"
+
+# delete the PR namespace
+kubectl delete ns $RENKU_NAMESPACE
+kubectl delete ns $RENKU_TMP_NAMESPACE
+
+# remove the gitlab app
+apps=$(curl -s https://dev.renku.ch/gitlab/api/v4/applications -H "private-token: ${GITLAB_TOKEN}" | jq -r ".[] | select(.application_name == \"${RENKU_RELEASE}\") | .id")
+for app in $apps
+do
+    curl -X DELETE https://dev.renku.ch/gitlab/api/v4/applications/${app} -H "private-token: ${GITLAB_TOKEN}"
+done
