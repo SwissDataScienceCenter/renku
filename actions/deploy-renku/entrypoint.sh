0042c55fc440b5710d06534f533fc9b193f05efa
Rok Roškar <roskarr@ethz.ch>
2021-01-22 12:23:53 +0100

    chore: modify CD workflow for deployment script (#1867)

--- actions/deploy-renku/entrypoint.sh
+++ actions/deploy-renku/entrypoint.sh
@@ -9,7 +9,14 @@ echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 # set up kube context and values file
 echo "$RENKUBOT_KUBECONFIG" > "$KUBECONFIG" && chmod 400 "$KUBECONFIG"
 
+# set up the values file
+printf "%s" "$RENKU_VALUES" | sed "s/<replace>/${RENKU_RELEASE}/" > $RENKU_VALUES_FILE
+
+# enable anonymous notebooks
+yq w -i $RENKU_VALUES_FILE "global.anonymousSessions.enabled" "true"
+
 # register the GitLab app
+if [[ -n "$GITLAB_TOKEN" ]]; then
 gitlab_app=$(curl -s -X POST https://dev.renku.ch/gitlab/api/v4/applications \
                       -H "private-token: $GITLAB_TOKEN" \
                       --data "name=${RENKU_RELEASE}" \
@@ -18,32 +25,31 @@ gitlab_app=$(curl -s -X POST https://dev.renku.ch/gitlab/api/v4/applications \
 APP_ID=$(echo $gitlab_app | jq -r '.application_id')
 APP_SECRET=$(echo $gitlab_app | jq -r '.secret')
 
-# set up the values file
-printf "%s" "$RENKU_VALUES" | sed "s/<replace>/${RENKU_RELEASE}/" > values.yaml
-
 # gateway gitlab app/secret
-yq w -i values.yaml "gateway.gitlabClientId" "$APP_ID"
-yq w -i values.yaml "gateway.gitlabClientSecret" "$APP_SECRET"
+yq w -i $RENKU_VALUES_FILE "gateway.gitlabClientId" "$APP_ID"
+yq w -i $RENKU_VALUES_FILE "gateway.gitlabClientSecret" "$APP_SECRET"
 
 # jupyterhub gitlab app/secret
-yq w -i values.yaml "notebooks.jupyterhub.auth.gitlab.clientId" "$APP_ID"
-yq w -i values.yaml "notebooks.jupyterhub.auth.gitlab.clientSecret" "$APP_SECRET"
-
-# enable anonymous notebooks
-yq w -i values.yaml "global.anonymousSessions.enabled" "true"
+yq w -i $RENKU_VALUES_FILE "notebooks.jupyterhub.auth.gitlab.clientId" "$APP_ID"
+yq w -i $RENKU_VALUES_FILE "notebooks.jupyterhub.auth.gitlab.clientSecret" "$APP_SECRET"
+fi
 
 # create the namespace in a Rancher project
+if [[ -n "$RANCHER_PROJECT_ID" ]]; then
 curl -s -H "Authorization: Bearer $RENKUBOT_RANCHER_BEARER_TOKEN" \
         -X POST \
         -d "name=${RENKU_NAMESPACE}" \
         -d "projectId=${RANCHER_PROJECT_ID}" \
         'https://rancher.renku.ch/v3/cluster/c-l6jt4/namespaces'
 
+if [[ $RENKU_ANONYMOUS_SESSIONS = "true" ]]; then
 curl -s -H "Authorization: Bearer $RENKUBOT_RANCHER_BEARER_TOKEN" \
         -X POST \
         -d "name=${RENKU_TMP_NAMESPACE}" \
         -d "projectId=${RANCHER_PROJECT_ID}" \
         'https://rancher.renku.ch/v3/cluster/c-l6jt4/namespaces'
+fi
+fi
 
 # deploy renku - reads config from environment variables
 helm repo add renku https://swissdatasciencecenter.github.io/helm-charts
