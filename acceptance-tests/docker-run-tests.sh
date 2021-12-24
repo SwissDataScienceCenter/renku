3cfbdf9097d6d344c37767d8f5c8552d64500578
Tasko Olevski <olevski90@gmail.com>
2021-12-24 12:48:34 -0500

    squashme: catch and re-emit exit code for tests

--- acceptance-tests/docker-run-tests.sh
+++ acceptance-tests/docker-run-tests.sh
@@ -40,3 +40,5 @@ if [ $TESTS_RC -eq 0 ]
 then
   MC_HOST_bucket="https://${RENKU_TEST_S3_ACCESS_KEY}:${RENKU_TEST_S3_SECRET_KEY}@${RENKU_TEST_S3_HOST}" mc rm --recursive --force bucket/${RENKU_TEST_S3_BUCKET}/${RENKU_TEST_S3_FILENAME}/
 fi
+
+exit TESTS_RC
