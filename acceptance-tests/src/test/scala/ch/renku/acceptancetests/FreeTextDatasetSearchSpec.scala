5854749b8d1f8c9bda173c77533b98591c110f63
Jakub Chrobasik <jakub.chrobasik@gmail.com>
2022-01-05 09:24:42 +0100

    chore: year updated in all acceptance-tests preambles (#2413)

--- acceptance-tests/src/test/scala/ch/renku/acceptancetests/FreeTextDatasetSearchSpec.scala
+++ acceptance-tests/src/test/scala/ch/renku/acceptancetests/FreeTextDatasetSearchSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Swiss Data Science Center (SDSC)
+ * Copyright 2022 Swiss Data Science Center (SDSC)
  * A partnership between École Polytechnique Fédérale de Lausanne (EPFL) and
  * Eidgenössische Technische Hochschule Zürich (ETHZ).
  *
