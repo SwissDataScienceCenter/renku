ea91fc54748da04bd6da67b53af4ffc939ca9db0
David Kunzmann <joke1196@users.noreply.github.com>
2022-01-11 10:50:45 +0100

    test: corrected fetch of current renku version in acceptance test (#2410)
    * test: corrected jupyter notebook spec to correctly grab the current renku version
    * test: fixed selection of ordering on dataset search
    
    Co-authored-by: Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
    Co-authored-by: David Kunzmann <davidkunzmann@gmail.com>

--- acceptance-tests/src/test/scala/ch/renku/acceptancetests/pages/DatasetsPage.scala
+++ acceptance-tests/src/test/scala/ch/renku/acceptancetests/pages/DatasetsPage.scala
@@ -38,6 +38,22 @@ object DatasetsPage
     find(cssSelector("#searchButton")) getOrElse fail("Datasets -> search button cannot be found")
   }
 
+  def orderByDropdownMenu(
+      currentOrdering:  DatasetSearchOrdering = DatasetSearchOrdering.ProjectsCount
+  )(implicit webDriver: WebDriver): WebElement =
+    eventually {
+      findAll(cssSelector("button.dropdown-toggle")).find(elem => elem.getText == currentOrdering.value) getOrElse fail(
+        s"Datasets -> current ordering ${currentOrdering.value} button not found"
+      )
+    }
+
+  def orderByDropdownItem(datasetOrdering: DatasetSearchOrdering)(implicit webDriver: WebDriver): WebElement =
+    eventually {
+      findAll(cssSelector("button.dropdown-item")).find(elem => elem.getText == datasetOrdering.value) getOrElse fail(
+        s"Datasets -> order by ${datasetOrdering.value} button not found"
+      )
+    }
+
   def searchResultLinks(implicit webDriver: WebDriver): List[WebElement] = eventually {
     findAll(cssSelector(".rk-search-result-card > a > div > div.title")).toList
   }
@@ -57,5 +73,19 @@ object DatasetsPage
       waitIfBouncing
     case _ =>
       sleep(1 second)
+  }
+  trait DatasetSearchOrdering {
+    def value: String
+  }
+  object DatasetSearchOrdering {
+    case object Title extends DatasetSearchOrdering {
+      val value = "Title"
+    }
+    case object Date extends DatasetSearchOrdering {
+      val value = "Date"
+    }
+    case object ProjectsCount extends DatasetSearchOrdering {
+      val value = "Projects count"
+    }
   }
 }
