5419e9b69e50630bf1516aa7227cb994850e7de9
Tasko Olevski <olevski90@gmail.com>
2022-01-18 08:39:30 +0100

    chore(test): add new test and cleanup actions (#2411)
    * chore(ci): continuously mirror test results
    
    * chore(ci): use new actions for testing and cleanup
    
    Co-authored-by: Rok Roškar <rok.roskar@sdsc.ethz.ch>

--- acceptance-tests/docker-run-tests.sh
+++ acceptance-tests/docker-run-tests.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 if [ "$#" -lt "1" ]; then
   TARGET="test"
@@ -7,12 +7,10 @@ else
 fi
 
 cd /tests
-echo "Target: " $TARGET
-sbt -Dsbt.color=always -Dsbt.supershell=false -Dsbt.global.base=/tests/.sbt/global -Dsbt.boot.directory=/tests/.sbt/boot/ -Dsbt.coursier.home=/tests/.sbt/coursier/ scalafmtAll "$TARGET"
+mkdir -p target
 
-TESTS_RC=$?
 
-if [ ! -z $RENKU_TEST_S3_HOST ] && [ $TESTS_RC != 0 ]; then
+if [ ! -z $RENKU_TEST_S3_HOST ]; then
   if [ -z $RENKU_TEST_S3_ACCESS_KEY ] || [ -z $RENKU_TEST_S3_SECRET_KEY ] || \
   [ -z $RENKU_TEST_S3_BUCKET ] || [ -z $RENKU_TEST_S3_FILENAME ]; then
     echo "[ERROR] To upload the tests to a S3 bucket then\
@@ -21,16 +19,30 @@ if [ ! -z $RENKU_TEST_S3_HOST ] && [ $TESTS_RC != 0 ]; then
     must all be specified as environment variables. Aborting."
     exit 1
   fi
-  mkdir test-artifacts
-  cp target/*.png test-artifacts
-  cp target/*.log test-artifacts
-  rm -rf target/20*/.renku/cache
-  rm -rf target/20*/.gitattributes
-  cp -r target/20* test-artifacts
-  tar czvf tests-artifacts.tgz test-artifacts
-  mv tests-artifacts.tgz ${RENKU_TEST_S3_FILENAME}.tgz
-
-  export MC_HOST_bucket="https://${RENKU_TEST_S3_ACCESS_KEY}:${RENKU_TEST_S3_SECRET_KEY}@${RENKU_TEST_S3_HOST}"
-  mc cp ${RENKU_TEST_S3_FILENAME}.tgz bucket/${RENKU_TEST_S3_BUCKET}
-  exit $TESTS_RC
+  # mirror the test results folder in the background
+  # the sleep command ensures the tests have started before mirroring starts
+  echo "Will upload test artefacts from target to https://XXXX:XXXX@${RENKU_TEST_S3_HOST} bucket/${RENKU_TEST_S3_BUCKET}/${RENKU_TEST_S3_FILENAME}"
+  sleep 60 && MC_HOST_bucket="https://${RENKU_TEST_S3_ACCESS_KEY}:${RENKU_TEST_S3_SECRET_KEY}@${RENKU_TEST_S3_HOST}" mc mirror --overwrite --remove --watch --exclude "scala*/*" --exclude "streams/*" --exclude "task-temp-directory/*" --exclude "global-logging/*"  --exclude "target/20*/.gitattributes" --exclude "target/20*/.renku/cache" target bucket/${RENKU_TEST_S3_BUCKET}/${RENKU_TEST_S3_FILENAME}/ 2> /dev/null 1> /dev/null &
+  MIRROR_JOB_ID=$(echo $!)
 fi
+
+echo "Target: " $TARGET
+sbt -Dsbt.color=always -Dsbt.supershell=false -Dsbt.global.base=/tests/.sbt/global -Dsbt.boot.directory=/tests/.sbt/boot/ -Dsbt.coursier.home=/tests/.sbt/coursier/ scalafmtAll "$TARGET"
+TESTS_RC=$?
+echo "Tests completed with exit code $TESTS_RC"
+
+# cleanup the background mirroring job
+if [ ! -z $MIRROR_JOB_ID ]
+then
+  echo "Terminating test artifacts background mirroring job."
+  kill -15 $MIRROR_JOB_ID
+fi
+
+# if tests pass then remove the bucket
+if [ $TESTS_RC -eq 0 ]
+then
+  echo "The tests PASSED with return code $TESTS_RC, removing the test artifacts at $RENKU_TEST_S3_BUCKET/$RENKU_TEST_S3_FILENAME."
+  MC_HOST_bucket="https://${RENKU_TEST_S3_ACCESS_KEY}:${RENKU_TEST_S3_SECRET_KEY}@${RENKU_TEST_S3_HOST}" mc rm --recursive --force bucket/${RENKU_TEST_S3_BUCKET}/${RENKU_TEST_S3_FILENAME}/
+fi
+
+exit $TESTS_RC
