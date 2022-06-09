6480c290b1dbaa311c0e09fc6507ee6a06e6cd2b
Rok Roškar <roskarr@ethz.ch>
2022-06-09 11:58:25 +0200

    Update docs/conf.py
    Co-authored-by: Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
     "sphinxcontrib.spelling",
-    'sphinxcontrib.mermaid',
+    "sphinxcontrib.mermaid",
     "sphinx_rtd_theme",
     "sphinx_copybutton",
     "sphinx_panels",
