11ec94559eef14bb09f856aa03190ffdc0379552
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2022-12-05 14:09:15 +0100

    chore: try devenv

---
+++ .devenv.flake.nix
@@ -0,0 +1,51 @@
+{
+  inputs = {
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv?dir=src/modules";
+  } // (if builtins.pathExists ./.devenv/devenv.json 
+       then (builtins.fromJSON (builtins.readFile ./.devenv/devenv.json)).inputs
+       else {});
+
+  outputs = { nixpkgs, ... }@inputs:
+    let
+      pkgs = import nixpkgs { system = "aarch64-darwin"; };
+      lib = pkgs.lib;
+      devenv = if builtins.pathExists ./.devenv/devenv.json
+        then builtins.fromJSON (builtins.readFile ./.devenv/devenv.json)
+        else {};
+      toModule = path:
+        if lib.hasPrefix "./" path
+        then ./. + (builtins.substring 1 255 path) + "/devenv.nix"
+        else if lib.hasPrefix "../" path 
+        then throw "devenv: ../ is not supported for imports"
+        else let
+          paths = lib.splitString "/" path;
+          name = builtins.head paths;
+          input = inputs.${name} or (throw "Unknown input ${name}");
+          subpath = "/${lib.concatStringsSep "/" (builtins.tail paths)}";
+          devenvpath = "${input}/" + subpath + "/devenv.nix";
+          in if (!devenv.inputs.${name}.flake or true) && builtins.pathExists devenvpath
+             then devenvpath
+             else throw (devenvpath + " file does not exist for input ${name}.");
+      project = pkgs.lib.evalModules {
+        specialArgs = inputs // { inherit inputs pkgs; };
+        modules = [
+          (inputs.devenv.modules + /top-level.nix)
+          { devenv.cliVersion = "0.4"; }
+        ] ++ (map toModule (devenv.imports or [])) ++ [
+          ./devenv.nix
+          (devenv.devenv or {})
+          (if builtins.pathExists ./devenv.local.nix then ./devenv.local.nix else {})
+        ];
+      };
+      config = project.config;
+    in {
+      packages."aarch64-darwin" = {
+        ci = pkgs.runCommand "ci" {} ("ls " + toString config.ci + " && touch $out");
+        inherit (config) info procfileScript procfileEnv procfile;
+      };
+      devShell."aarch64-darwin" = config.shell;
+    };
+}
---
+++ devenv.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs.git
+    pkgs.kubernetes-helm
+    ];
+
+  enterShell = ''
+    hello
+    git --version
+    helm version
+  '';
+
+  # https://devenv.sh/languages/
+  languages.nix.enable = true;
+  languages.python.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo hello from $GREET";
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.shellcheck.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # devcontainer
+  devcontainer.enable = true;
+}
