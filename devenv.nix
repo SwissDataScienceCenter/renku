f8ccde8cf61866a3c80d28fb806a435a7daeb80d
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2022-12-07 15:40:25 +0100

    chore: add python config

--- devenv.nix
+++ devenv.nix
@@ -1,5 +1,49 @@
 { pkgs, ... }:
+let
+  mach-nix = import (builtins.fetchGit {
+    url = "https://github.com/DavHau/mach-nix";
+    ref = "refs/tags/3.5.0";
+    # ref = "master";
+  }) {
+    pypiDataRev = "982b6cdf6552fb9296e1ade29cf65a2818cbbd6b";
+    pypiDataSha256 = "sha256:166y0li0namv6a8ik8qq79ibck4w74x0wgypn9r7sqbb2wvcvcf3";
+    python = "python39";
+  };
+  python-packages = mach-nix.mkPython {
+    requirements = ''
+      pip
+      powerline-status
+      psutil
+      setuptools_rust
+      virtualenv
+      plantweb==1.2.1
+      sphinx>=4.1
+      sphinxcontrib-mermaid==0.7.1
+      sphinxcontrib-napoleon==0.7
+      sphinx-click==4.3.0
+      sphinx-copybutton==0.5.1
+      sphinx-panels
+      renku-sphinx-theme==0.2.2
+      sphinxcontrib-spelling==7.*
+      sphinxcontrib-websupport==1.2.4
+      pyenchant
+      renku
+      lockfile
+    '';
+    providers = {
+    _default = "wheel,sdist,nixpkgs";
+    psutil = "sdist,nixpkgs";
+    pyenchant = "nixpkgs";
+    };
 
+    # packagesExtra = [ mach-nix.buildPythonPackage {
+    #   src = ./docs/renku-python;
+    #   requirementsExtra = "lockfile";
+    #   }
+    # ];
+    _.pyenchant.nativeBuildInputs.mod = pySelf: self: oldVal: oldVal ++ [pkgs.hunspellDicts.en-us];
+};
+in
 {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
@@ -8,17 +52,21 @@
   packages = [
     pkgs.git
     pkgs.kubernetes-helm
+    python-packages
+    pkgs.hunspell
+    pkgs.hunspellDicts.en-us
     ];
 
   enterShell = ''
     hello
     git --version
+    echo "helm version"
     helm version
+    echo renku v$(renku --version)
   '';
 
   # https://devenv.sh/languages/
   languages.nix.enable = true;
-  languages.python.enable = true;
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
