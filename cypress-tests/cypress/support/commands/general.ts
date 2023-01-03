202aef2398d81c10c6b92e24c3f5bb315b274775
Andrea Cordoba <dandrea.cordoba@gmail.com>
2023-01-03 17:04:15 +0100

    test: update cypress test for new projects search

--- cypress-tests/cypress/support/commands/general.ts
+++ cypress-tests/cypress/support/commands/general.ts
@@ -14,8 +14,9 @@ export type SearchForProjectProps = {
   namespace: string
 }
 export function searchForProject(props: SearchForProjectProps) {
-  cy.get("input[placeholder='Search or jump to...']").type(props.name).type("{enter}");
-  cy.contains(`${props.namespace}/${props.name}`).should("be.visible");
+  cy.visit("/search");
+  cy.get("input[placeholder='Search...']").type(props.name).type("{enter}");
+  cy.get("[data-cy='list-card-title']").contains(props.name);
 }
 
 function dataCy(value: string) {
