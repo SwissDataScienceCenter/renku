69b85cfba1675d0242407637af0c711d54ba25d0
Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>
2023-03-08 10:25:42 +0100

    chore: add comment about tests ordering
    Co-authored-by: Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>

--- cypress-tests/cypress.config.ts
+++ cypress-tests/cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   e2e: {
     baseUrl: process.env.BASE_URL || "https://dev.renku.ch",
     specPattern: [
+      // Verify infrastructure before running any other tests
       "cypress/e2e/verifyInfrastructure.cy.ts",
       "cypress/e2e/*.{js,jsx,ts,tsx}"
     ],
