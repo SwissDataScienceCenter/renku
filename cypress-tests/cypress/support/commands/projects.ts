e6e27e1b1cb7894e4abff8a7241c6d52fa8799f7
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2023-03-21 11:17:43 +0100

    tests(cypress): update to support UI 3.4.0 (#2983)
    Co-authored-by: Chandrasekhar Ramakrishnan <cramakri@ethz.ch>
    Co-authored-by: Lorenzo Cavazzi <43481553+lorenzo-cavazzi@users.noreply.github.com>

--- cypress-tests/cypress/support/commands/projects.ts
+++ cypress-tests/cypress/support/commands/projects.ts
@@ -88,18 +88,16 @@ function getProjectPageLink(identifier: ProjectIdentifier, subpage: string) {
 function visitAndLoadProject(identifier: ProjectIdentifier, skipOutdated = false) {
   // load project and wait for the relevant resources to be loaded
   cy.intercept("/ui-server/api/user").as("getUser");
-  cy.intercept("/ui-server/api/graphql").as("getProjects");
   cy.intercept("/ui-server/api/renku/*/datasets.list*").as("getDatasets");
   let versionInvoked = false;
   cy.intercept("/ui-server/api/renku/renku/version", req => { versionInvoked = true; }).as("getVersion");
   cy.visitProject(identifier);
   cy.wait("@getUser", { timeout: TIMEOUTS.long });
-  cy.wait("@getProjects", { timeout: TIMEOUTS.long });
   if (versionInvoked)
     cy.wait("@getVersion", { timeout: TIMEOUTS.long });
   if (!skipOutdated)
     cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
-  
+
   // Other elements are re-rendered at this point; waiting 1 sec helps preventing "unmounted" errors
   // eslint-disable-next-line cypress/no-unnecessary-waiting
   cy.wait(1000);
