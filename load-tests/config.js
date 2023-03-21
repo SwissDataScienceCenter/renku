730bd7570d481aa203841bfd553a0a272b12f098
Tasko Olevski <tasko.olevski@sdsc.ethz.ch>
2023-03-21 15:48:36 +0100

    squashme: add defaults for configuration

--- load-tests/config.js
+++ load-tests/config.js
@@ -1,16 +1,8 @@
-function getEnv(name) {
-  let val = __ENV[name];
-  if ((val === undefined) || (val === null)) {
-      throw new Error("missing env var for " + name);
-  }
-  return val;
-}
-
-export const baseUrl = getEnv("BASE_URL");
-export const gitUrl = getEnv("GIT_URL");
-export const registryDomain = getEnv("REGISTRY_DOMAIN");
+export const baseUrl = __ENV["BASE_URL"] || "https://dev.renku.ch";
+export const gitUrl = __ENV["GIT_URL"] || "https://gitlab.dev.renku.ch";
+export const registryDomain = __ENV["REGISTRY_DOMAIN"] || "repository.dev.renku.ch";
 export const oldRenkuTemplateRef = __ENV["OLD_RENKU_TEMPLATE_REF"] || "0.4.0";
 export const concurentUsers = __ENV["CONCURRENT_USERS"] || "3";
 
-const credentialsFile = getEnv("CREDENTIALS_FILE");
+const credentialsFile = __ENV["CREDENTIALS_FILE"] || "/credentials.json";
 export const credentials = JSON.parse(open(credentialsFile));
