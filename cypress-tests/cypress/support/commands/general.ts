edd4bc55cfde4db050814292b730caac085f7a41
Lorenzo <lorenzo.cavazzi.tech@gmail.com>
2023-09-05 10:54:04 +0200

    chore: update helper functions

--- cypress-tests/cypress/support/commands/general.ts
+++ cypress-tests/cypress/support/commands/general.ts
@@ -4,21 +4,23 @@ export const validateLogin = {
   validate() {
     // This returns 401 when not properly logged in
     cy.request("/ui-server/api/user");
-  }
+  },
 };
 
 export const getIframe = (selector: string) => {
   // https://github.com/cypress-io/cypress-example-recipes/blob/master/examples/blogs__iframes/cypress/support/e2e.js
   cy.log("getIframeBody");
   cy.get(selector, { timeout: TIMEOUTS.long })
-    .its("0.contentDocument.body").should("not.be.empty");
-  return cy.get(selector, { timeout: TIMEOUTS.long })
-    .its("0.contentDocument").then(cy.wrap);
+    .its("0.contentDocument.body")
+    .should("not.be.empty");
+  return cy
+    .get(selector, { timeout: TIMEOUTS.long })
+    .its("0.contentDocument")
+    .then(cy.wrap);
 };
 
 function getDataCy(value: string, exist: true) {
-  if (exist)
-    return cy.get(`[data-cy=${value}]`).should("exist");
+  if (exist) return cy.get(`[data-cy=${value}]`).should("exist");
   return cy.get(`[data-cy=${value}]`);
 }
 
--- cypress-tests/cypress/support/commands/sessions.ts
+++ cypress-tests/cypress/support/commands/sessions.ts
@@ -1,4 +1,3 @@
-
 import { TIMEOUTS } from "../../../config";
 import { fullProjectIdentifier } from "./projects";
 import type { ProjectIdentifier } from "./projects";
@@ -7,19 +6,28 @@ function startSession(identifier: ProjectIdentifier) {
   const id = fullProjectIdentifier(identifier);
   cy.visit(`/projects/${id.namespace}/${id.name}/sessions/new`);
   cy.get(".btn-rk-green", { timeout: TIMEOUTS.long })
-    .contains("Start session").should("be.visible").should("be.enabled").click();
+    .contains("Start session")
+    .should("be.visible")
+    .should("be.enabled")
+    .click();
 
-  cy.contains("Connecting with your session", { timeout: TIMEOUTS.long }).should("be.visible");
-  cy.contains("Connecting with your session", { timeout: TIMEOUTS.vlong }).should("not.exist");
+  cy.contains("Connecting with your session", {
+    timeout: TIMEOUTS.long,
+  }).should("be.visible");
+  cy.contains("Connecting with your session", {
+    timeout: TIMEOUTS.vlong,
+  }).should("not.exist");
 }
 
 function waitForImageToBuild(identifier: ProjectIdentifier) {
   const id = fullProjectIdentifier(identifier);
   cy.visit(`/projects/${id.namespace}/${id.name}/sessions/new`);
-  cy.get(".btn-rk-green", { timeout: TIMEOUTS.vlong }).should("be.visible").should("be.enabled");
+  cy.get(".btn-rk-green", { timeout: TIMEOUTS.vlong })
+    .should("be.visible")
+    .should("be.enabled");
 }
 
-const stopAllSessionsForProject = (identifier: ProjectIdentifier)=> {
+const stopAllSessionsForProject = (identifier: ProjectIdentifier) => {
   const id = fullProjectIdentifier(identifier);
   cy.intercept("/ui-server/api/notebooks/servers*").as("getSessions");
   cy.visit(`/projects/${id.namespace}/${id.name}/sessions`);
@@ -40,15 +48,18 @@ const stopAllSessionsForProject = (identifier: ProjectIdentifier)=> {
     }
   });
   cy.contains("No currently running sessions.", { timeout: TIMEOUTS.long });
-  cy.dataCy("go-back-button").click();
+  cy.getDataCy("go-back-button").click();
 };
 
 export const stopSessionFromIframe = () => {
-  cy.intercept({ method: "DELETE", url: /.*\/api\/notebooks\/servers\/.*/, times: 1 }, (req) => {
-    req.continue((res) => {
-      expect(res.statusCode).to.eq(204);
-    });
-  });
+  cy.intercept(
+    { method: "DELETE", url: /.*\/api\/notebooks\/servers\/.*/, times: 1 },
+    (req) => {
+      req.continue((res) => {
+        expect(res.statusCode).to.eq(204);
+      });
+    }
+  );
   cy.get(`[data-cy="stop-session-button"]`).should("be.visible").click();
   cy.get("div.modal-session").should("be.visible").should("not.be.empty");
   cy.get(`[data-cy="stop-session-modal-button"]`).should("be.visible").click();
@@ -61,7 +72,6 @@ export default function registerSessionCommands() {
   Cypress.Commands.add("stopSessionFromIframe", stopSessionFromIframe);
   Cypress.Commands.add("stopAllSessionsForProject", stopAllSessionsForProject);
 }
-
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
