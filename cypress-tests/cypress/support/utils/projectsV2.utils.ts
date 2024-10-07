7bd810aa5cef0e07c88db92f391ea6a572ff27ca
Andrea Cordoba <43388408+andre-code@users.noreply.github.com>
2024-10-07 10:58:36 +0200

    test: add dashboard v2 e2e tests (#3781)
    Co-authored-by: Renku Bot <renku@datascience.ch>
    Co-authored-by: Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>

---
+++ cypress-tests/cypress/support/utils/projectsV2.utils.ts
@@ -0,0 +1,55 @@
+export type ProjectIdentifierV2 = {
+  slug: string;
+  namespace?: string;
+  id?: string;
+};
+
+export interface NewProjectV2Props extends ProjectIdentifierV2 {
+  visibility?: "public" | "private";
+  name: string;
+}
+
+/** Get the namespace of the logged in user from the API. */
+export function getUserNamespaceAPIV2(): Cypress.Chainable<string | null> {
+  return cy.request({ failOnStatusCode: false, method: "GET", url: `api/data/namespaces?minimum_role=owner` })
+    .then((response) => {
+      if (response.status === 200) {
+        const userNamespace = response.body?.filter((namespace) => namespace.namespace_kind === "user");
+        return userNamespace && userNamespace.length > 0 ? userNamespace[0].slug : null;
+      }
+      return null;
+    });
+}
+
+/** Get a project by using only the API. */
+export function getProjectByNamespaceAPIV2(newProjectProps: ProjectIdentifierV2): Cypress.Chainable<any | null> {
+  return cy.request({ failOnStatusCode: false, method: "GET", url: `api/data/projects/${newProjectProps.namespace}/${newProjectProps.slug}` });
+}
+
+/** Create a project (if the project is missing) by using only the API. */
+export function createProjectIfMissingAPIV2(newProjectProps: NewProjectV2Props) {
+  return getProjectByNamespaceAPIV2(newProjectProps)
+    .then((response) => {
+      if (response.status != 200) {
+        return cy.request({
+          method: "POST",
+          url: "api/data/projects",
+          body: newProjectProps,
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+      } else {
+        return response.body;
+      }
+    });
+}
+
+/** Delete a project by using only the API. */
+export function deleteProjectFromAPIV2(projectIdentifier: ProjectIdentifierV2) {
+  return cy.request({
+    failOnStatusCode: false,
+    method: "DELETE",
+    url: `api/data/projects/${projectIdentifier.id}`,
+  });
+}
