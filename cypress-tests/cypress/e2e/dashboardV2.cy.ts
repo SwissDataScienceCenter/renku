dc9caa96904ee195157b8cd317ee6000765f0ad3
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2025-01-31 14:31:04 +0100

    refactor: change v2 urls

--- cypress-tests/cypress/e2e/dashboardV2.cy.ts
+++ cypress-tests/cypress/e2e/dashboardV2.cy.ts
@@ -21,7 +21,7 @@ describe("Dashboard v2 - Authenticated user", () => {
   };
 
   after(() => {
-    if (!projectTestConfig.projectAlreadyExists && projectIdentifier.id != null){
+    if (!projectTestConfig.projectAlreadyExists && projectIdentifier.id != null) {
       deleteProjectFromAPIV2(projectIdentifier);
       getProjectByNamespaceAPIV2(projectIdentifier).then((response) => {
         expect(response.status).to.equal(404);
@@ -47,22 +47,23 @@ describe("Dashboard v2 - Authenticated user", () => {
           name: `${prefixProjectTitle} ${projectIdentifier.slug}`,
           namespace,
           slug: projectIdentifier.slug,
-        }).then((project) => projectIdentifier.id=project.id)
-      } else {
-        cy.log('No user namespace found, project cannot be created.');
+        }).then((project) => projectIdentifier.id = project.id);
+      }
+      else {
+        cy.log("No user namespace found, project cannot be created.");
       }
     });
   });
 
   it("Can see own project on the dashboard", () => {
-    cy.visit("v2");
+    cy.visit("/");
     cy.getDataCy("dashboard-project-list").find("a").should("have.length.at.least", 1);
     cy.getDataCy("dashboard-project-list").find("a").should("contain.text", `${prefixProjectTitle} ${projectIdentifier.slug}`);
     cy.getDataCy("dashboard-project-list").find("a").should("contain.text", projectIdentifier.slug);
   });
 
   it("Can find project in the search results", () => {
-    cy.visit("v2");
+    cy.visit("/");
     cy.getDataCy("view-my-projects-btn").click();
     cy.getDataCy("search-card").should("have.length.at.least", 1);
     cy.getDataCy("search-card").should("contain.text", `${prefixProjectTitle} ${projectIdentifier.slug}`);
@@ -71,8 +72,7 @@ describe("Dashboard v2 - Authenticated user", () => {
 
 describe("Dashboard v2 - Non-Authenticated user", () => {
   it("Cannot see projects and groups on Dashboard when logged out", () => {
-    cy.visit("v2");
-    cy.getDataCy("user-container").should("be.visible");
-    cy.getDataCy("user-container").should("contain.text", "You are not logged in.");
+    cy.visit("/");
+    cy.getDataCy("user-container").should("not.exist");
   });
 });
