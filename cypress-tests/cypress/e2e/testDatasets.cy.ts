bddee94202e6a21e5de899f763e8db3684542634
Flora Thiebaut <flora.thiebaut@sdsc.ethz.ch>
2025-04-30 16:36:02 +0200

    try

--- cypress-tests/cypress/e2e/testDatasets.cy.ts
+++ cypress-tests/cypress/e2e/testDatasets.cy.ts
@@ -25,13 +25,6 @@ const projectIdentifier: ProjectIdentifier = {
 const sessionId = ["testDatasets", getRandomString()];
 
 describe("Basic datasets functionality", () => {
-  before(() => {
-    // Intercept listing datasets
-    cy.intercept("/ui-server/api/renku/*/datasets.list?git_url=*").as(
-      "listDatasets",
-    );
-  });
-
   beforeEach(() => {
     // Restore the session
     cy.session(
@@ -53,7 +46,7 @@ describe("Basic datasets functionality", () => {
   const keywords = ["test", "automated test", "Cypress test"];
   const description = "This is a test dataset form a Cypress tests";
 
-  it.skip("A new project should have no datasets", () => {
+  it("A new project should have no datasets", () => {
     const emptyProject: ProjectIdentifier = {
       name: generatorProjectName("testDatasets"),
       namespace: username,
@@ -61,19 +54,19 @@ describe("Basic datasets functionality", () => {
     cy.createProjectIfMissing({ templateName: "Python", ...emptyProject });
     cy.visitAndLoadProject(emptyProject);
     cy.getProjectSection("Datasets").click();
-    cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
     cy.contains("No datasets found for this project.", {
       timeout: TIMEOUTS.vlong,
     }).should("be.visible");
   });
 
-  it.skip("Create a dataset", () => {
+  it("Create a dataset", () => {
     const generatedDatasetName = generatorDatasetName("Dataset");
 
     // Reload page to clear the client-side cache
     cy.reload();
     cy.getProjectSection("Datasets").click();
-    cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
 
     // Create a dataset
     cy.get("#plus-dropdown").should("exist").click();
@@ -86,14 +79,14 @@ describe("Basic datasets functionality", () => {
     cy.get("div.ck.ck-editor__main div.ck.ck-content")
       .should("exist")
       .type(description);
-      cy.intercept("POST", "/ui-server/api/renku/*/datasets.create").as(
-        "createDataset",
-      );
+    cy.intercept("POST", "/ui-server/api/renku/*/datasets.create").as(
+      "createDataset",
+    );
     cy.getDataCy("submit-button").click();
     cy.get(".progress-box").should("be.visible");
 
     cy.wait("@createDataset", { timeout: TIMEOUTS.long });
-    cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
 
     // Check that the content is correct
     cy.getDataCy("dataset-title")
@@ -107,34 +100,38 @@ describe("Basic datasets functionality", () => {
     cy.contains(description).get(".renku-markdown").should("be.visible");
   });
 
-  it.only("Modify the dataset and search for it", () => {
+  it("Modify the dataset and search for it", () => {
     const generatedDatasetName = generatorDatasetName("Dataset");
 
-     // Reload page to clear the client-side cache
-     cy.reload();
-     cy.getProjectSection("Datasets").click();
-     cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    // Reload page to clear the client-side cache
+    cy.reload();
+    cy.getProjectSection("Datasets").click();
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
 
-        cy.get("#plus-dropdown").should("exist").click();
-        cy.get("#navbar-dataset-new").should("exist").click();
-        cy.getDataCy("input-title").type(generatedDatasetName.name);
+    // Create a dataset
+    cy.get("#plus-dropdown").should("exist").click();
+    cy.get("#navbar-dataset-new").should("exist").click();
+    cy.getDataCy("input-title").type(generatedDatasetName.name);
 
-        cy.getDataCy("input-keywords").type(
-          keywords.reduce((text, value) => `${text}${value}{enter}`, ""),
-        );
-        cy.get("div.ck.ck-editor__main div.ck.ck-content")
-          .should("exist")
-          .type(description);
-          cy.intercept("POST","/ui-server/api/renku/*/datasets.create").as("createDataset")
-        cy.getDataCy("submit-button").click();
-        cy.get(".progress-box").should("be.visible");
+    cy.getDataCy("input-keywords").type(
+      keywords.reduce((text, value) => `${text}${value}{enter}`, ""),
+    );
+    cy.get("div.ck.ck-editor__main div.ck.ck-content")
+      .should("exist")
+      .type(description);
+    cy.intercept("POST", "/ui-server/api/renku/*/datasets.create").as(
+      "createDataset",
+    );
+    cy.getDataCy("submit-button").click();
+    cy.get(".progress-box").should("be.visible");
 
-        cy.wait("@createDataset", { timeout: TIMEOUTS.long })
-          cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    cy.wait("@createDataset", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
 
     // Add a keyword and check it is visible
+    cy.visitAndLoadProject(projectIdentifier);
     cy.getProjectSection("Datasets").click();
-      cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
     cy.getDataCy("list-card-title")
       .contains(generatedDatasetName.name)
       .should("be.visible")
@@ -143,14 +140,17 @@ describe("Basic datasets functionality", () => {
       .contains("Dataset files")
       .should("be.visible");
     cy.getDataCy("edit-dataset-button").last().click();
-    const newKeyword = "additioanl keyword";
+    const newKeyword = "additional keyword";
     cy.getDataCy("input-keywords").type(`${newKeyword}{enter}`);
-    // TODO
+    cy.intercept("POST", "/ui-server/api/renku/*/datasets.edit").as(
+      "modifyDataset",
+    );
     cy.getDataCy("submit-button").click();
     cy.contains("Modifying dataset").should("be.visible");
-      cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
-    keywords.push(newKeyword);
-    for (const keyword of keywords) {
+    cy.wait("@modifyDataset", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
+    const updatedKeywords = [...keywords, newKeyword];
+    for (const keyword of updatedKeywords) {
       cy.getDataCy("entity-tag-list")
         .contains(`#${keyword}`)
         .should("be.visible");
@@ -162,23 +162,49 @@ describe("Basic datasets functionality", () => {
     cy.searchForDataset(generatedDatasetName.name);
   });
 
-  // it("Delete the dataset", () => {
-  //   cy.getProjectSection("Datasets").click();
-  //     cy.wait("@listDatasets", { timeout: TIMEOUTS.long });
-  //   cy.getDataCy("list-card-title")
-  //     .contains(generatedDatasetName.name)
-  //     .should("be.visible")
-  //     .click();
+  it("Delete the dataset", () => {
+    const generatedDatasetName = generatorDatasetName("Dataset");
 
-  //   cy.getDataCy("delete-dataset-button").click();
-  //   cy.contains("Are you sure you want to delete dataset").should("be.visible");
-  //   cy.get(".modal").contains("Delete dataset").click();
-  //   cy.get(".modal").contains("Deleting dataset...").should("be.visible");
+    // Reload page to clear the client-side cache
+    cy.reload();
+    cy.getProjectSection("Datasets").click();
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
 
-  //   // Check the dataset is gone after the project has been indexed
-  //   cy.waitMetadataIndexing();
-  //   // ! Currently, datasets don't disappear instantly because the UI uses a renku-core API
-  //   // ! We can leave this disabled until that's addressed
-  //   // cy.searchForDataset(generatedDatasetName.slug, false);
-  // });
+    // Create a dataset
+    cy.get("#plus-dropdown").should("exist").click();
+    cy.get("#navbar-dataset-new").should("exist").click();
+    cy.getDataCy("input-title").type(generatedDatasetName.name);
+
+    cy.getDataCy("input-keywords").type(
+      keywords.reduce((text, value) => `${text}${value}{enter}`, ""),
+    );
+    cy.get("div.ck.ck-editor__main div.ck.ck-content")
+      .should("exist")
+      .type(description);
+    cy.intercept("POST", "/ui-server/api/renku/*/datasets.create").as(
+      "createDataset",
+    );
+    cy.getDataCy("submit-button").click();
+    cy.get(".progress-box").should("be.visible");
+
+    cy.wait("@createDataset", { timeout: TIMEOUTS.long });
+    cy.wait("@getDatasets", { timeout: TIMEOUTS.long });
+
+    // cy.getDataCy("list-card-title")
+    //   .contains(generatedDatasetName.name)
+    //   .should("be.visible")
+    //   .click();
+
+    // Delete the dataset
+    cy.getDataCy("delete-dataset-button").click();
+    cy.contains("Are you sure you want to delete dataset").should("be.visible");
+    cy.get(".modal").contains("Delete dataset").click();
+    cy.get(".modal").contains("Deleting dataset...").should("be.visible");
+
+    // Check the dataset is gone after the project has been indexed
+    cy.waitMetadataIndexing();
+    // ! Currently, datasets don't disappear instantly because the UI uses a renku-core API
+    // ! We can leave this disabled until that's addressed
+    // cy.searchForDataset(generatedDatasetName.slug, false);
+  });
 });
