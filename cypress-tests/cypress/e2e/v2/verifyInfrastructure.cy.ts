b35f71bea71ba7925f19dd6cd025c52461981d3b
Tasko Olevski <olevski90@gmail.com>
2025-06-27 11:08:51 +0200

    squashme: minor fix

--- cypress-tests/cypress/e2e/v2/verifyInfrastructure.cy.ts
+++ cypress-tests/cypress/e2e/v2/verifyInfrastructure.cy.ts
@@ -47,7 +47,7 @@ describe("Verify the infrastructure is ready", () => {
     });
 
     // Search should return a list of items
-    const searchUrl = "/api/search/query";
+    const searchUrl = "/api/data/search/query";
     cy.request(searchUrl).then((resp) => {
       if (resp.status >= 400 || !("items" in resp.body))
         throw new Error("Search endpoints not working as expected.");
