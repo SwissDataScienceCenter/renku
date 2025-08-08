cbc9e14284ec55c739a6fbc2a25712d49e4184d6
Tasko Olevski <olevski90@gmail.com>
2025-08-08 15:45:20 +0200

    squashme: strip the path from the url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: url.toString(),
+  url: `http://${url.host}`,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: url.pathname,
