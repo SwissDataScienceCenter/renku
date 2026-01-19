b99c3f196f7be18fb3307aa48ecd8cde0f6c0dc4
Rok Roškar <rok.roskar@sdsc.ethz.ch>
2026-01-19 15:25:05 +0100

    chore: add path regex replacement

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -186,10 +186,10 @@ const config: Config = {
       // externalUrlRegex: 'external\\.com|domain\\.com',
 
       // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-      // replaceSearchResultPathname: {
-      //   from: '/docs/', // or as RegExp: /\/docs\//
-      //   to: '/',
-      // },
+      replaceSearchResultPathname: {
+        from: '/en/[a-zA-Z0-9_-]+/',
+        to: '/',
+      },
 
       // Optional: Algolia search parameters
       // searchParameters: {},
