3b214fdb2e28be7cf8d631773655d4233bb5c16e
Chandrasekhar Ramakrishnan <ciyer@users.noreply.github.com>
2026-04-13 12:03:20 +0200

    fix problems PR

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -75,9 +75,13 @@ const config: Config = {
   projectName: "renku", // Usually your repo name.
 
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "warn",
 
-  markdown: { mermaid: true },
+  markdown: {
+    mermaid: true,
+    hooks: {
+      onBrokenMarkdownLinks: "warn",
+    },
+  },
 
   themes: ["@docusaurus/theme-mermaid"],
 
