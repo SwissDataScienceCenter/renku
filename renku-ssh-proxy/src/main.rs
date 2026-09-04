684a637d08687f43fef1d25d5462297f6994bc1f
Eike <eikek@posteo.de>
2026-09-04 10:04:06 +0200

    ssh proxy first version
    - allow to connect to a fixed target host

--- renku-ssh-proxy/src/main.rs
+++ renku-ssh-proxy/src/main.rs
@@ -1,6 +1,5 @@
 use renku_ssh_proxy::Settings;
 use renku_ssh_proxy::buildinfo;
-use renku_ssh_proxy::echoserver;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
@@ -8,7 +7,7 @@ async fn main() -> color_eyre::Result<()> {
 
     let args: Vec<String> = std::env::args().collect();
 
-    // Intercept before clap parses → skips all validation
+    // Intercept before clap parses to skip validation and exit early
     if args.iter().any(|a| a == "--version" || a == "-V") {
         let info = buildinfo::BuildInfo::default();
         println!("{}", info);
@@ -21,7 +20,10 @@ async fn main() -> color_eyre::Result<()> {
         .filter_level(settings.log_level.log_level_filter())
         .init();
 
-    log::info!("Hello! Try to connect to port 2222, like ssh -p 2222 <the-host>");
-    let _ = echoserver::do_proxy(&settings).await;
+    println!(
+        "Proxy server running. Try to connect to {}",
+        settings.listen
+    );
+    let _ = renku_ssh_proxy::serve_proxy(&settings).await;
     Ok(())
 }
--- renku-ssh-proxy/src/proxy_server.rs
+++ renku-ssh-proxy/src/proxy_server.rs
@@ -1,14 +1,25 @@
 use std::collections::HashMap;
 use std::sync::Arc;
 
+use crate::Settings;
 use color_eyre::eyre::{OptionExt, Result};
-use russh::client;
 use russh::keys::ssh_key::PublicKey;
 use russh::keys::{PrivateKeyWithHashAlg, load_secret_key};
-use russh::server;
-use russh::{Channel, ChannelId, Pty, Sig};
+use russh::server::Server as _;
+use russh::{Channel, ChannelId, ChannelMsg, Pty, Sig};
+use russh::{client, server};
 use std::path::PathBuf;
+use tokio::net::TcpListener;
 use tokio::sync::mpsc;
+
+/// Creates and runs a proxy server
+pub async fn serve_proxy(settings: &Settings) -> Result<()> {
+    let mut ph = ProxyHandler::new().with_target(settings.target.clone());
+    let socket = TcpListener::bind(settings.listen).await?;
+    let server = ph.run_on_socket(settings.ssh_server_config.clone(), &socket);
+    server.await?;
+    Ok(())
+}
 
 #[derive(Clone, Debug)]
 pub struct Target {
@@ -19,11 +30,11 @@ pub struct Target {
     pub expected_host_key: Option<PublicKey>,
 }
 
-struct UpstreamHandler {
+struct TargetHandler {
     expected_host_key: Option<PublicKey>,
 }
 
-impl client::Handler for UpstreamHandler {
+impl client::Handler for TargetHandler {
     type Error = color_eyre::eyre::Error;
 
     async fn check_server_key(
@@ -38,7 +49,8 @@ impl client::Handler for UpstreamHandler {
     }
 }
 
-pub enum Cmd {
+#[derive(Debug, Clone)]
+pub enum SshMsg {
     Data(Vec<u8>),
     Signal(Sig),
     Pty {
@@ -51,13 +63,21 @@ pub enum Cmd {
     },
     Shell,
     Eof,
+    Exec(Vec<u8>),
+    WindowChange {
+        cols: u32,
+        rows: u32,
+        pixw: u32,
+        pixh: u32,
+    },
+    Subsystem(String),
 }
 
 #[derive(Clone)]
 pub struct ProxyHandler {
     pub target: Option<Target>,
-    upstream: Option<Arc<client::Handle<UpstreamHandler>>>,
-    channels: HashMap<ChannelId, mpsc::Sender<Cmd>>,
+    upstream: Option<Arc<client::Handle<TargetHandler>>>,
+    channels: HashMap<ChannelId, mpsc::Sender<SshMsg>>,
 }
 
 impl ProxyHandler {
@@ -69,11 +89,16 @@ impl ProxyHandler {
         }
     }
 
-    async fn connect_target(&mut self) -> Result<Arc<client::Handle<UpstreamHandler>>> {
+    pub fn with_target(mut self, target: Target) -> Self {
+        self.target = Some(target);
+        self
+    }
+
+    async fn connect_target(&mut self) -> Result<Arc<client::Handle<TargetHandler>>> {
         if let Some(h) = &self.upstream {
             return Ok(h.clone());
         }
-        log::debug!("Connecting to target host: {:?}", &self.target);
+        log::debug!("Connecting to target host: {:?}", self.target);
         let target = self
             .target
             .clone()
@@ -82,20 +107,20 @@ impl ProxyHandler {
         let mut handle = russh::client::connect(
             config,
             (target.host.as_str(), target.port),
-            UpstreamHandler {
+            TargetHandler {
                 expected_host_key: target.expected_host_key,
             },
         )
         .await?;
         log::info!(
             "Created client handle. Loading key from {:?}",
-            &target.key_path
+            target.key_path
         );
         let key = load_secret_key(&target.key_path, None)?;
         let halg = client::Handle::best_supported_rsa_hash(&handle)
             .await?
-            .unwrap_or_else(|| Some(ssh_key::HashAlg::Sha256));
-        log::info!("Using hash-alg: {:?}", halg);
+            .unwrap_or(Some(ssh_key::HashAlg::Sha256));
+        log::debug!("Using hash-alg with target host: {:?}", halg);
         let auth = handle
             .authenticate_publickey(
                 &target.user,
@@ -110,6 +135,18 @@ impl ProxyHandler {
         self.upstream = Some(handle.clone());
         Ok(handle)
     }
+
+    async fn forward(&self, channel: &ChannelId, msg: SshMsg) -> Result<()> {
+        if let Some(tx) = self.channels.get(channel) {
+            let result = tx.send(msg).await;
+            if let Err(err) = result {
+                log::error!("Error forwarding ssh message: {:?}", err);
+            }
+        } else {
+            log::warn!("No channel available for {:?}", channel);
+        }
+        Ok(())
+    }
 }
 
 impl server::Handler for ProxyHandler {
@@ -120,14 +157,16 @@ impl server::Handler for ProxyHandler {
         _user: &str,
         _public_key: &ssh_key::PublicKey,
     ) -> std::prelude::v1::Result<server::Auth, Self::Error> {
-        self.target = Some(Target {
-            host: "eknet.org".to_string(),
-            port: 22,
-            user: "eike".to_string(),
-            key_path: PathBuf::from("/home/sdsc/workspace/renku/renku-ssh-proxy/id_rsa_key"),
-            expected_host_key: None,
-        });
-        Ok(server::Auth::Accept)
+        if self.target.is_none() {
+            log::warn!("No target host set!");
+            Ok(server::Auth::Reject {
+                proceed_with_methods: None,
+                partial_success: false,
+            })
+        } else {
+            // todo: reach out to data_services
+            Ok(server::Auth::Accept)
+        }
     }
 
     async fn channel_open_session(
@@ -140,11 +179,11 @@ impl server::Handler for ProxyHandler {
         let client_id = channel.id();
         let upstream = self.connect_target().await?;
         let up_channel = upstream.channel_open_session().await?;
-        let (tx, rx) = mpsc::channel::<Cmd>(64);
+        let (tx, rx) = mpsc::channel::<SshMsg>(64);
         self.channels.insert(client_id, tx);
         log::debug!(
             "Connected. Open ssh session to target host: {:?}",
-            &self.target
+            self.target
         );
 
         let serve_handle = session.handle();
@@ -161,10 +200,7 @@ impl server::Handler for ProxyHandler {
         _session: &mut server::Session,
     ) -> std::prelude::v1::Result<(), Self::Error> {
         log::debug!("Sending data...");
-        if let Some(tx) = self.channels.get(&channel) {
-            let _ = tx.send(Cmd::Data(data.to_vec())).await;
-        }
-        Ok(())
+        self.forward(&channel, SshMsg::Data(data.to_vec())).await
     }
 
     async fn shell_request(
@@ -173,9 +209,7 @@ impl server::Handler for ProxyHandler {
         session: &mut server::Session,
     ) -> Result<(), Self::Error> {
         log::debug!("Requesting shell...");
-        if let Some(tx) = self.channels.get(&channel) {
-            let _ = tx.send(Cmd::Shell).await;
-        }
+        let _ = self.forward(&channel, SshMsg::Shell).await;
         session.channel_success(channel)?;
         Ok(())
     }
@@ -186,9 +220,16 @@ impl server::Handler for ProxyHandler {
         _session: &mut server::Session,
     ) -> std::prelude::v1::Result<(), Self::Error> {
         log::debug!("Sending eof...");
-        if let Some(tx) = self.channels.get(&channel) {
-            let _ = tx.send(Cmd::Eof).await;
-        }
+        self.forward(&channel, SshMsg::Eof).await
+    }
+
+    async fn channel_close(
+        &mut self,
+        channel: ChannelId,
+        _session: &mut server::Session,
+    ) -> std::prelude::v1::Result<(), Self::Error> {
+        log::debug!("Sending channel_close...");
+        self.channels.remove(&channel);
         Ok(())
     }
 
@@ -198,10 +239,19 @@ impl server::Handler for ProxyHandler {
         sig: Sig,
         _session: &mut server::Session,
     ) -> Result<(), Self::Error> {
-        log::debug!("Sending signal {:?}...", &sig);
-        if let Some(tx) = self.channels.get(&channel) {
-            let _ = tx.send(Cmd::Signal(sig)).await;
-        }
+        log::debug!("Sending signal {:?}...", sig);
+        self.forward(&channel, SshMsg::Signal(sig)).await
+    }
+
+    async fn exec_request(
+        &mut self,
+        channel: ChannelId,
+        data: &[u8],
+        session: &mut server::Session,
+    ) -> std::prelude::v1::Result<(), Self::Error> {
+        log::debug!("Sending exec...");
+        self.forward(&channel, SshMsg::Exec(data.to_vec())).await?;
+        session.channel_success(channel)?;
         Ok(())
     }
 
@@ -216,52 +266,131 @@ impl server::Handler for ProxyHandler {
         modes: &[(russh::Pty, u32)],
         _session: &mut server::Session,
     ) -> std::prelude::v1::Result<(), Self::Error> {
-        if let Some(tx) = self.channels.get(&channel) {
-            let _ = tx
-                .send(Cmd::Pty {
-                    term: term.to_string(),
-                    cols: col_width,
-                    rows: row_height,
-                    pixw: pix_width,
-                    pixh: pix_height,
-                    modes: modes.to_vec(),
-                })
-                .await?;
-        }
+        log::debug!("Sending pty request...");
+        self.forward(
+            &channel,
+            SshMsg::Pty {
+                term: term.to_string(),
+                cols: col_width,
+                rows: row_height,
+                pixw: pix_width,
+                pixh: pix_height,
+                modes: modes.to_vec(),
+            },
+        )
+        .await
+    }
+
+    async fn window_change_request(
+        &mut self,
+        channel: ChannelId,
+        col_width: u32,
+        row_height: u32,
+        pix_width: u32,
+        pix_height: u32,
+        session: &mut server::Session,
+    ) -> std::prelude::v1::Result<(), Self::Error> {
+        log::debug!("Sending window_change request...");
+        self.forward(
+            &channel,
+            SshMsg::WindowChange {
+                cols: col_width,
+                rows: row_height,
+                pixw: pix_width,
+                pixh: pix_height,
+            },
+        )
+        .await?;
+        session.channel_success(channel)?;
+        Ok(())
+    }
+
+    async fn subsystem_request(
+        &mut self,
+        channel: ChannelId,
+        name: &str,
+        session: &mut server::Session,
+    ) -> std::prelude::v1::Result<(), Self::Error> {
+        log::debug!("Sending subsystem request...");
+        self.forward(&channel, SshMsg::Subsystem(name.to_string()))
+            .await?;
+        session.channel_success(channel)?;
         Ok(())
     }
 }
 
+/// Forwards ssh messages received from the channel to the client handler.
 async fn proxy_channel(
-    up: Channel<client::Msg>,
-    mut rx: mpsc::Receiver<Cmd>,
-    _client: server::Handle,
-    _client_id: ChannelId,
+    mut up: Channel<client::Msg>,
+    mut rx: mpsc::Receiver<SshMsg>,
+    client: server::Handle,
+    client_id: ChannelId,
 ) {
     loop {
         tokio::select! {
-            cmd = rx.recv() => match cmd {
-                Some(Cmd::Data(data)) => {
-                    let _ = up.data(&data[..]).await;
+            cmd = rx.recv() => {
+                log::trace!("Received ssh-msg {:?}, sending upstream...", cmd);
+                match cmd {
+                    Some(SshMsg::Data(data)) => {
+                        let _ = up.data(&data[..]).await;
+                    }
+                    Some(SshMsg::Pty{term, cols, rows, pixh, pixw, modes}) => {
+                        let _ = up.request_pty(true, &term, cols, rows, pixw, pixh, &modes).await;
+                    }
+                    Some(SshMsg::Signal(sig)) => {
+                        let _ = up.signal(sig).await;
+                    }
+                    Some(SshMsg::Shell) => {
+                        let _ = up.request_shell(true).await;
+                    }
+                    Some(SshMsg::Eof) => {
+                        let _ = up.eof().await;
+                    }
+                    Some(SshMsg::Exec(cmd)) => {
+                        let _ = up.exec(true, cmd).await;
+                    }
+                    Some(SshMsg::WindowChange { cols, rows, pixw, pixh }) => {
+                        let _ = up.window_change(cols, rows, pixw, pixh).await;
+                    }
+                    Some(SshMsg::Subsystem(name)) => {
+                        let _ = up.request_subsystem(true, name).await;
+                    }
+                    None => {
+                        let _ = up.eof().await;
+                        let _ = up.close().await;
+                        break;
+                    }
                 }
-                Some(Cmd::Pty{term, cols, rows, pixh, pixw, modes}) => {
-                    let _ = up.request_pty(true, &term, cols, rows, pixw, pixh, &modes).await;
+            },
+
+            msg = up.wait() => {
+                log::trace!("Received msg from upstream, send to local client: {:?}", msg);
+                match msg {
+                    Some(ChannelMsg::Data { data }) => {
+                        let _ = client.data(client_id, data).await;
+                    }
+                    Some(ChannelMsg::ExtendedData { data, ext }) => {
+                        let _ = client.extended_data(client_id,ext, data).await;
+                    },
+                    Some(ChannelMsg::Eof) => {
+                        let _ = client.eof(client_id).await;
+                    },
+                    Some(ChannelMsg::ExitStatus { exit_status }) => {
+                        let _ = client.exit_status_request(client_id, exit_status).await;
+                    },
+                    Some(ChannelMsg::Close) => {
+                        let _ = client.close(client_id).await;
+                        break;
+                    },
+                    None => {
+                        let _ = client.close(client_id).await;
+                        break;
+                    },
+                    Some(msg) => {
+                        log::warn!("No upstream message handler available for {:?}", msg);
+                    }
                 }
-                Some(Cmd::Signal(sig)) => {
-                    let _ = up.signal(sig).await;
-                }
-                Some(Cmd::Shell) => {
-                    let _ = up.request_shell(true).await;
-                }
-                Some(Cmd::Eof) => {
-                    let _ = up.eof().await;
-                }
-                None => {
-                    let _ = up.eof().await;
-                    let _ = up.close().await;
-                    break;
-                }
-            }
+            },
         }
     }
 }
@@ -273,5 +402,11 @@ impl server::Server for ProxyHandler {
     }
     fn handle_session_error(&mut self, error: <Self::Handler as russh::server::Handler>::Error) {
         log::error!("Session error: {error:#?}");
+    }
+}
+
+impl Drop for ProxyHandler {
+    fn drop(&mut self) {
+        self.channels.clear();
     }
 }
