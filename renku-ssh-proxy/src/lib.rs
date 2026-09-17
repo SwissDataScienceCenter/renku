da35ea784a32155bd97a521378c46d2757900d4c
Salim Kayal <salim.kayal@idiap.ch>
2026-09-17 17:30:12 +0200

    feat(ssh-proxy): add Keycloak client-credentials token provider

--- renku-ssh-proxy/src/lib.rs
+++ renku-ssh-proxy/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod buildinfo;
 pub(crate) mod config;
 pub(crate) mod data_services;
+pub(crate) mod keycloak;
 mod proxy_server;
 pub use config::Settings;
 pub use config::generate_completions;
